0a344c011e5509fb53bc917173d8ab4b45375e7e
Nikolay Morozov <joxterrr@gmail.com>
2019-03-30 22:41:06 +0300

    instal gulp, pug, hello world

---
+++ build.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+var pug = require("gulp-pug");
+
+function buildHTML() {
+  return gulp
+    .src("pages/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("static/"));
+}
+
+gulp.task("views", buildHTML);
+
+exports.default = buildHTML;
