e224be779369c39f8f3f9f2ad616788241d23351
Nikolay Morozov <joxterrr@gmail.com>
2019-04-17 14:40:38 +0300

    функции работы с куками и url

--- pages/urlModules.js
+++ pages/urlModules.js
@@ -27,3 +27,65 @@ export function stringifyModules(modules, userSelect) {
 
   return url;
 }
+
+export function getCookie(name) {
+  const matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function setCookie(name, value, options = {}) {
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+export function deleteCookie(name) {
+  setCookie(name, "", {
+    expires: -1
+  });
+}
+
+function parseQueryString(query) {
+  var vars = query.split("&");
+  var params = {};
+
+  vars.forEach(variable => {
+    var [key, value] = variable.split("=");
+
+    if (key) {
+      if (value === undefined) {
+        value = true;
+      }
+      params[decodeURIComponent(key)] = decodeURIComponent(value);
+    }
+  });
+
+  return params;
+}
