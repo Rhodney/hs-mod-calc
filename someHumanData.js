6511c52204a8a43a8ab94fc3709cb605aad6d13d
Nikolay Morozov <joxterrr@gmail.com>
2020-02-22 23:17:41 +0300

    all requriments

--- someHumanData.js
+++ someHumanData.js
@@ -62,55 +62,46 @@ var modulesByTypes = {
 
 // All
 
-// Blueprints Required
-// Unlock Price
-// Research Time
-// Additional Hydrogen Use
-// Activation Cost
-
+// --Blueprints Required
+// --Unlock Price
+// --Research Time
+// --Additional Hydrogen Use
+// --Activation Cost
+// --Install Price
 const paramsNames = {
   TransportCapacity: [
     // Additional Slots
-    // Additional Hydrogen Use
   ],
   ShipmentComputer: [
     // Bonus Reward
-    // Additional Hydrogen Use
   ],
   Trader: [
     // Trade Boost
     // Effect Duration
     // Shipment Reward Bonus
-    // Activation Cost
   ],
   Rush: [
     // Speed Increase Per Shipment
-    // Additional Hydrogen Use
   ],
   TradeBurst: [
     // Threshold
     // Bonus Reward
     // Effect Duration
-    // Activation Cost
   ],
   ShipmentDrone: [
     // Total Cargo Slots
     // Bonus Per Additional Shipment
     // Max Bonus
-    // Activation Cost
   ],
   Offload: [
     // Payoff
     // Effect Duration
-    // Activation Cost
   ],
   ShipmentBeam: [
     // Max Shipments
-    // Activation Cost
   ],
   Entrust: [
     // Effect Range
-    // Activation Cost
   ],
   Dispatch: [
     // Preparation Time Per Item
@@ -122,8 +113,8 @@ const paramsNames = {
   /////////////////////////////////////////////
   MiningBoost: [
     // Effect Duration
-    //Effect Duration (White Star)
-    //Mining Speed
+    // Effect Duration (White Star)
+    // Mining Speed
   ],
   MineralStorageCapacity: [
     // Additional Storage
@@ -140,28 +131,229 @@ const paramsNames = {
   ],
   MiningUnity: [
     // Speed Increase Per Player
-    //Effect Duration
-    //Effect Duration (White Star)
+    // Effect Duration
+    // Effect Duration (White Star)
   ],
   Crunch: [
     // Max Amount
   ],
   Genesis: [
     // Max New Asteroids
-    //Max New Hydrogen
+    // Max New Hydrogen
   ],
   HydroRocket: [
     // Required Hydrogen
-    //Hull Strength
-    //Explosion Damage
-    //Explosion Range
+    // Hull Strength
+    // Explosion Damage
+    // Explosion Range
   ],
   MiningDrone: [
     // Hull Strength
     // Mining Speed
     // Total Hydrogen Capacity
   ],
-  /////////////////
+  /////////////// weapons
+  WeakBattery: [
+    // Damage Per Second
+    // Damage Per Hour (White Star)
+  ],
+  Battery: [
+    // Damage Per Second
+    // Damage Per Hour (White Star)
+  ],
+  Laser: [
+    // Damage Per Second
+    // Maximum Damage Per Second
+    // Time to Maximum Damage (Red Star)
+    // Damage Per Hour (White Star)
+    // Maximum Damage Per Hour (White Star)
+    // Time to Maximum Damage (White Star)
+    // Time to Maximum Damage (Blue Star)
+  ],
+  MassBattery: [
+    // Damage Per Second
+    // Damage Per Hour (White Star)
+    // Max Targets
+  ],
+  DualLaser: [
+    // Damage Per Second
+    // Maximum Damage Per Second
+    // Time to Maximum Damage
+    // Damage Per Hour (White Star)
+    // Maximum Damage Per Hour (White Star)
+    // Time to Maximum Damage (White Star)
+    // Time to Maximum Damage (Blue Star)
+    // Max Targets
+  ],
+  Barrage: [
+    // Damage Per Second
+    // Additional Damage Per Enemy
+    // Maximum Damage
+    // Damage Per Hour (White Star)
+    // Additional Damage Per Enemy (White Star)
+    // Maximum Damage (White Star)
+  ],
+  DartLauncher: [
+    // Hull Strength
+    // Explosion Damage
+    // Explosion Range
+    // Effect Range
+  ],
+  /////////////////   shield
+  WeakShield: [
+    // Shield Strength
+  ],
+  StandardShield: [
+    // Shield Strength
+    // Speed Increase
+  ],
+  PassiveShield: [
+    // Shield Strength
+  ],
+  StrongShield: [
+    // Shield Strength
+  ],
+  MirrorShield: [
+    // Shield Strength
+    // Damage Mirrored
+  ],
+  BlastShield: [
+    // Shield Strength
+    // Effect Range
+  ],
+  AreaShield: [
+    // Shield Strength
+    // Effect Range
+  ],
+  /////////////////   support
+  EMP: [
+    // Effect Range
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Duration (Blue Star)
+  ],
+  Teleport: [
+    // Effect Range
+    // Effect Range (White Star)
+    // Effect Range (Blue Star)
+    // Maximum Travel Efficiency (Red Star)
+  ],
+  RedStarExtender: [
+    // Life Extension
+  ],
+  Repair: [
+    // Effect Range
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Duration (Blue Star)
+    // HP Repair Rate
+    // HP Repair Rate (White Star)
+    // Total HP Repaired
+    // Total HP Repaired (Blue Star)
+  ],
+  TimeWarp: [
+    // Effect Range
+    // Real Time Effect Duration
+    // Listed Effect Duration
+    // Real Time Effect Duration (White Star)
+    // Listed Effect Duration (White Star)
+    // Time Factor
+  ],
+  Unity: [
+    // Damage Increase Per Player
+    // Effect Duration
+    // Effect Duration (White Star)
+  ],
+  Sanctuary: [],
+  Stealth: [
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Duration (Blue Star)
+  ],
+  Fortify: [
+    // Damage Reduction
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Duration (Blue Star)
+  ],
+  Impulse: [
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Duration (Blue Star)
+  ],
+  AlphaRocket: [
+    // Hull Strength
+    // Explosion Damage
+    // Explosion Range
+  ],
+  Salvage: [
+    // Hull Repaired Per Destroyed Ship
+    // Hull Repaired Per Destroyed Ship (White Star)
+  ],
+  Supress: [
+    // Effect Range
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Range (Blue Star)
+  ],
+  Destiny: [
+    // Effect Range
+    // Area Damage
+    // Area Damage (White Star)
+    // Area Damage (Blue Star)
+  ],
+  Barrier: [
+    // Effect Range
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Range (Blue Star)
+    // Effect Duration (Blue Star)
+  ],
+  Vengeance: [
+    // Hull Strength Threshold
+    // Effect Range
+    // Area Damage
+    // Area Damage (White Star)
+    // Area Damage (Blue Star)
+  ],
+  DeltaRocket: [
+    // Hull Strength
+    // Explosion Damage
+    // Explosion Range
+  ],
+  Leap: [
+    // Activation Delay
+    // Activation Delay (White Star)
+    // Disable Time
+    // Disable Time (White Star)
+  ],
+  Bond: [
+    // Effect Range
+    // Effect Duration
+    // Effect Duration (White Star)
+    // Effect Range
+    // Effect Duration (Blue Star)
+  ],
+  AlphaDrone: [
+    // Damage Per Second
+    // Damage Per Hour (White Star)
+    // Hull Strength
+  ],
+  Suspend: [
+    // Effect Range
+    // Real Time Effect Duration
+    // Listed Effect Duration
+    // Real Time Effect Duration (White Star)
+    // Listed Effect Duration (White Star)
+    // Time Factor
+  ],
+  OmegaRocket: [
+    // Hull Strength
+    // Explosion Damage
+    // Explosion Range
+    // Neutralized Damage
+    // Neutralized Damage Range
+  ],
 };
 
 module.exports = {
